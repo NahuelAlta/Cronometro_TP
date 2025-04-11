bd905ddc45cd685f116eace0592f395cd57160ea
NahuelAlta <nahuel.altamirano.6@hotmail.com>
2025-04-11 19:57:54 -0300

    Eventos_cronometro_v1

--- main/blink_example_main.c
+++ main/blink_example_main.c
@@ -8,9 +8,6 @@
 #include "fonts.h"
 #include "ili9341.h"
 
-#define Corriendo   1
-#define Parado      0
-
 #define LED_ROJO        0   
 #define LED_VERDE       1
 
@@ -18,28 +15,80 @@
 #define BOTON_TC1   8
 #define BOTON_TC2   9
 
-#define DIGITO_ANCHO     60
+#define Interrupcion_TC0 (1 << 1)
+#define Interrupcion_TC1 (1 << 2)
+#define Interrupcion_TC2 (1 << 3)
+#define Cambio_min (1 << 4)
+#define Cambio_seg (1 << 5)
+#define Cambio_dec (1 << 6)
+#define Resetear_Ev (1 << 7)
+#define Pantalla_Lap (1 << 8)
+
+#define DIGITO_ANCHO     30
 #define DIGITO_ALTO      100
+#define DECIMA_ANCHO     30
+#define DECIMA_ALTO      80
+#define LAP_ANCHO        20
+#define LAP_ALTO         50
 #define DIGITO_ENCENDIDO ILI9341_RED
 #define DIGITO_APAGADO   0x3800
 #define DIGITO_FONDO     ILI9341_BLACK
 
-uint32_t Conteo = 0;
+uint32_t Minutos = 0;
+uint32_t Decimas = 0;
+uint32_t Segundos = 0;
 
-uint8_t Estado_global = Parado;
+uint8_t Estado_Global=0;
 
 typedef struct Cronometro_s {
-    SemaphoreHandle_t Global_Conteo; //Va a controlar el acceso a la variable de Conteo
-    SemaphoreHandle_t Estado_Cronometro; //Va a controlar el acceso a la variable de Estado_global
-    panel_t panel_lcd_dec;
-    panel_t panel_lcd_min;
-    panel_t panel_lcd_seg;
+    uint8_t Tope;
+    uint32_t *puntero_variable; //Va a controlar el acceso a la variable de Conteo
+    uint32_t Delay;
+    uint32_t Bits_de_cambio;
+    SemaphoreHandle_t Semáforo_variable; //Va a controlar el acceso a la variable de Estado_global
+    SemaphoreHandle_t Estado_Cronometro;
+    SemaphoreHandle_t Semaforo_UART;
+    EventGroupHandle_t Eventos_task;
+    panel_t Panel_grupo;
+    
 }* Cronometro_t;
 
+typedef struct Pantalla_Lap_s {
+    panel_t Panel_lap1;
+    panel_t Panel_lap2;
+    panel_t Panel_lap3;
+    panel_t Panel_lap4;
+} *Pantalla_Lap_t;
+
+typedef struct Control_teclas_cronometro_s {
+    EventGroupHandle_t  Evento;
+    SemaphoreHandle_t   Estado_Cronometro;
+    SemaphoreHandle_t   Semaforo_segundos;
+    SemaphoreHandle_t   Semaforo_minutos;
+    SemaphoreHandle_t   Semaforo_decimas;
+} *Control_teclas_t;
+
+void Handler_interrupcion_pin_TC0(void *args){
+    EventGroupHandle_t Evento = (EventGroupHandle_t) args;
+    xEventGroupSetBitsFromISR(Evento,Interrupcion_TC0,pdFALSE);
+}
+void Handler_interrupcion_pin_TC1(void *args){
+    EventGroupHandle_t Evento = (EventGroupHandle_t) args;
+    xEventGroupSetBitsFromISR(Evento,Interrupcion_TC1,pdFALSE);
+}
+void Handler_interrupcion_pin_TC2(void *args){
+    EventGroupHandle_t Evento = (EventGroupHandle_t) args;
+    xEventGroupSetBitsFromISR(Evento,Interrupcion_TC2,pdFALSE);
+}
 static void Parpadeo_led_verde (void *parameters){
+    SemaphoreHandle_t Estado_G = (SemaphoreHandle_t) parameters;
+    uint32_t Estado_local=0;
     static uint8_t estado=1;
     while(1){
-        if (Estado_global){
+        xSemaphoreTake(Estado_G,portMAX_DELAY);
+        Estado_local = Estado_Global;
+        xSemaphoreGive(Estado_G);
+        if (Estado_G){
             gpio_set_level(LED_VERDE,estado);
             estado^=1;
             vTaskDelay(250/portTICK_PERIOD_MS);
@@ -51,60 +100,31 @@ static void Parpadeo_led_verde (void *parameters){
     }
 }
 
-static void Chequeo_botones (void *parameters){
-    Cronometro_t parametros = (Cronometro_t) parameters;
-    while(1){
-        if (gpio_get_level(BOTON_TC0)==0){
-            vTaskDelay(50/portTICK_PERIOD_MS);
-            while(!gpio_get_level(BOTON_TC0)){}
-            xSemaphoreTake(parametros->Estado_Cronometro,portMAX_DELAY);
-                switch(Estado_global){
-                    case Parado:
-                    Estado_global = Corriendo;
-                    xSemaphoreGive(parametros->Estado_Cronometro);
-                    gpio_set_level(LED_ROJO,0);
-                    break;
-                    case Corriendo:
-                    Estado_global = Parado;
-                    xSemaphoreGive(parametros->Estado_Cronometro);
-                    gpio_set_level(LED_VERDE,0);
-                    gpio_set_level(LED_ROJO,1);
-                    break;                    
-                }
-        }
-        else if (gpio_get_level(BOTON_TC1)==0){
-            vTaskDelay(50/portTICK_PERIOD_MS);
-            while(!gpio_get_level(BOTON_TC1)){}
-            xSemaphoreTake(parametros->Estado_Cronometro,portMAX_DELAY);
-            if ((gpio_get_level(BOTON_TC1)==0) && Estado_global==Parado){
-                Conteo = 0;
-            }
-            xSemaphoreGive(parametros->Estado_Cronometro);
+static void Start_Stop_Cronometro (void *parameters){
 
-        }
-        else if (gpio_get_level(BOTON_TC2)==0){
-            if (gpio_get_level(BOTON_TC2)==0){
-                //printf("Minutos: %u/nSegundos: %u/nDecimas: %u",Minutos,Segundos,Decimas);
-            }
-            while(!gpio_get_level(BOTON_TC2)){}
-            vTaskDelay(50/portTICK_PERIOD_MS);
-        }
-        vTaskDelay(100/portTICK_PERIOD_MS);
-    }
 }
-
+static void Reset_Cronometro (void *parameters){
+    
+}
+static void Obtener_Lap (void *parameters){
+    
+}
 static void Contador (void *parameters){
     Cronometro_t parametros = (Cronometro_t) parameters;
     static TickType_t Tiempo_inicial = 0;
     Tiempo_inicial = xTaskGetTickCount();
     while(1){
         xSemaphoreTake(parametros->Estado_Cronometro,portMAX_DELAY);
-        if (Estado_global){
+        if (Estado_Global){
             xSemaphoreGive(parametros->Estado_Cronometro);
-            xSemaphoreTake(parametros->Global_Conteo,portMAX_DELAY);
-            Conteo+=1;
-            xSemaphoreGive(parametros->Global_Conteo);
-            vTaskDelayUntil(&Tiempo_inicial,pdMS_TO_TICKS(100));
+            xSemaphoreTake(parametros->Semáforo_variable,portMAX_DELAY);
+            *(parametros->puntero_variable)+=1;
+            if (parametros->Tope == *(parametros->puntero_variable)){
+                *(parametros->puntero_variable)=0;
+            }
+            xSemaphoreGive(parametros->Semáforo_variable);
+            xEventGroupSetBits(parametros->Eventos_task,parametros->Bits_de_cambio);
+            vTaskDelayUntil(&Tiempo_inicial,pdMS_TO_TICKS(parametros->Delay));
         }
         else{
             xSemaphoreGive(parametros->Estado_Cronometro);
@@ -115,69 +135,93 @@ static void Contador (void *parameters){
 
 static void Actualizar_pantalla(void *parameters){
     Cronometro_t parametros = (Cronometro_t) parameters;
-    static uint16_t Conteo_previo=0;
-    static uint32_t Minutos = 0;
-    static uint32_t Segundos = 0;
-    static uint32_t Decimas = 0;
-    static uint32_t Minutos_prev = 0;
-    static uint32_t Segundos_prev = 0;
+    EventBits_t Bits_evento;
+    uint8_t Decena=0;
+    uint8_t Unidad=0;
     while(1){
-        xSemaphoreTake(parametros->Global_Conteo,portMAX_DELAY);
-        if (Conteo_previo!=Conteo){
-            Conteo_previo=Conteo;
-            xSemaphoreGive(parametros->Global_Conteo);
-            Decimas=Conteo%10;
-            Segundos=Conteo/10;
-            Minutos=Segundos/60; 
-            DibujarDigito(parametros->panel_lcd_dec, 0, Decimas);
-            if (Segundos_prev!=Segundos){
-                Segundos_prev=Segundos;
-                uint8_t decena_seg=(Segundos%60)/10;
-                uint8_t unidad_seg=(Segundos%60)%10;
-                DibujarDigito(parametros->panel_lcd_seg, 0, decena_seg);
-                DibujarDigito(parametros->panel_lcd_seg, 1, unidad_seg);
-            }
-            if (Minutos_prev!=Minutos){
-                Minutos_prev=Minutos;
-                DibujarDigito(parametros->panel_lcd_min, 0, Minutos);  
-            }
-        }
-        else{
-            xSemaphoreGive(parametros->Global_Conteo);
-        }
-        vTaskDelay(50/portTICK_PERIOD_MS);
+        Bits_evento = xEventGroupWaitBits(
+            parametros->Eventos_task,    
+            parametros->Bits_de_cambio,  
+            pdTRUE,         
+            pdFALSE,        
+            portMAX_DELAY);
+        xSemaphoreTake(parametros->Semáforo_variable,portMAX_DELAY);
+        Decena=*(parametros->puntero_variable)/10;
+        Unidad=*(parametros->puntero_variable)%10;
+        xSemaphoreGive(parametros->Semáforo_variable);
+        xSemaphoreTake(parametros->Semaforo_UART,portMAX_DELAY);
+        DibujarDigito(parametros->Panel_grupo, 0, Decena);
+        DibujarDigito(parametros->Panel_grupo, 1, Unidad);
+        xSemaphoreGive(parametros->Semaforo_UART);
         }
 }
 
 void app_main (void){
-    SemaphoreHandle_t  Semaforo_Conteo; 
+    SemaphoreHandle_t  Semaforo_Decimas; 
+    SemaphoreHandle_t  Semaforo_Segundos; 
+    SemaphoreHandle_t  Semaforo_Minutos; 
     SemaphoreHandle_t  Semaforo_Estado_Cronometro;
-    Semaforo_Conteo    =xSemaphoreCreateMutex();
-    Semaforo_Estado_Cronometro    =xSemaphoreCreateMutex();
-    static struct Cronometro_s Control_temporal;  
-    Control_temporal.Global_Conteo=Semaforo_Conteo; //Hace referencia a la variable de Conteo. Actualizado en la task de "Contador"
-    Control_temporal.Estado_Cronometro=Semaforo_Estado_Cronometro; //Hace referencia a la variable Estado, Corriendo o Parado
+    SemaphoreHandle_t  Periférico_UART;
 
+    Semaforo_Decimas                =xSemaphoreCreateMutex();
+    Semaforo_Segundos               =xSemaphoreCreateMutex();
+    Semaforo_Minutos                =xSemaphoreCreateMutex();
+    Semaforo_Estado_Cronometro      =xSemaphoreCreateMutex();
+    Periférico_UART                 =xSemaphoreCreateMutex();
+
+    EventGroupHandle_t Grupo_eventos;
+    Grupo_eventos = xEventGroupCreate();
+
+    static struct Cronometro_s Control_temporal[]={
+        {.Bits_de_cambio = Cambio_dec, .Delay=100, .puntero_variable=&Decimas, .Tope=10},
+        {.Bits_de_cambio = Cambio_min, .Delay=60000, .puntero_variable=&Minutos, .Tope=60},
+        {.Bits_de_cambio = Cambio_seg, .Delay=1000, .puntero_variable=&Segundos, .Tope=60}
+    };  
+    Control_temporal[0].Estado_Cronometro=Semaforo_Estado_Cronometro;
+    Control_temporal[1].Estado_Cronometro=Semaforo_Estado_Cronometro;
+    Control_temporal[2].Estado_Cronometro=Semaforo_Estado_Cronometro;
+
+    Control_temporal[0].Semáforo_variable=Semaforo_Decimas;
+    Control_temporal[1].Semáforo_variable=Semaforo_Minutos;
+    Control_temporal[2].Semáforo_variable=Semaforo_Segundos;
+
+    Control_temporal[0].Semaforo_UART=Periférico_UART;
+    Control_temporal[1].Semaforo_UART=Periférico_UART;
+    Control_temporal[2].Semaforo_UART=Periférico_UART;
+
+    Control_temporal[0].Eventos_task=Grupo_eventos;
+    Control_temporal[1].Eventos_task=Grupo_eventos;
+    Control_temporal[2].Eventos_task=Grupo_eventos;
+    
     ILI9341Init();
     ILI9341Rotate(ILI9341_Landscape_1);
 
-    Control_temporal.panel_lcd_min=CrearPanel(10, 60, 1, DIGITO_ALTO, DIGITO_ANCHO, DIGITO_ENCENDIDO, DIGITO_APAGADO, DIGITO_FONDO);
-    Control_temporal.panel_lcd_seg=CrearPanel(90, 60, 2, DIGITO_ALTO, DIGITO_ANCHO, DIGITO_ENCENDIDO, DIGITO_APAGADO, DIGITO_FONDO);
-    Control_temporal.panel_lcd_dec=CrearPanel(230, 60, 1, DIGITO_ALTO, DIGITO_ANCHO, DIGITO_ENCENDIDO, DIGITO_APAGADO, DIGITO_FONDO);
+    Control_temporal[0].Panel_grupo=CrearPanel(50, 125, 2, DECIMA_ALTO, DECIMA_ANCHO, DIGITO_ENCENDIDO, DIGITO_APAGADO, DIGITO_FONDO);
+    Control_temporal[1].Panel_grupo=CrearPanel(10, 10, 2, DIGITO_ALTO, DIGITO_ANCHO, DIGITO_ENCENDIDO, DIGITO_APAGADO, DIGITO_FONDO);
+    Control_temporal[2].Panel_grupo=CrearPanel(90, 10, 1, DIGITO_ALTO, DIGITO_ANCHO, DIGITO_ENCENDIDO, DIGITO_APAGADO, DIGITO_FONDO);
    
-    DibujarDigito(Control_temporal.panel_lcd_dec, 0, 0);
-    DibujarDigito(Control_temporal.panel_lcd_seg, 0, 0);
-    DibujarDigito(Control_temporal.panel_lcd_seg, 1, 0);
-    DibujarDigito(Control_temporal.panel_lcd_min, 0, 0);
-    ILI9341DrawFilledCircle(80, 150, 5, DIGITO_ENCENDIDO);
-    ILI9341DrawFilledCircle(220, 150 , 5, DIGITO_ENCENDIDO);
+    DibujarDigito(Control_temporal[0].Panel_grupo, 0, 0);
+    DibujarDigito(Control_temporal[0].Panel_grupo, 1, 0);
+    DibujarDigito(Control_temporal[1].Panel_grupo, 0, 0);
+    DibujarDigito(Control_temporal[1].Panel_grupo, 1, 0);
+    DibujarDigito(Control_temporal[2].Panel_grupo, 0, 0);
+    DibujarDigito(Control_temporal[2].Panel_grupo, 1, 0);
+
+    static struct Control_teclas_cronometro_s Teclas_control;
+    Teclas_control.Estado_Cronometro=Semaforo_Estado_Cronometro;
+    Teclas_control.Evento=Grupo_eventos;
+    Teclas_control.Semaforo_decimas=Semaforo_Decimas;
+    Teclas_control.Semaforo_segundos=Semaforo_Segundos;
+    Teclas_control.Semaforo_minutos=Semaforo_Minutos;
+    
+    ILI9341DrawFilledCircle(75, 105, 5, DIGITO_ENCENDIDO);
 
     gpio_config_t io_conf_int = {};
     io_conf_int.pin_bit_mask =
         ((1ULL << BOTON_TC0) | (1ULL << BOTON_TC1) | (1ULL << BOTON_TC2));
     io_conf_int.mode = GPIO_MODE_INPUT;
     io_conf_int.pull_up_en = true;
-    io_conf_int.intr_type = GPIO_INTR_DISABLE;
+    io_conf_int.intr_type = GPIO_INTR_POSEDGE;
     gpio_config(&io_conf_int);
 
     gpio_config_t io_conf_int2 = {};
@@ -189,9 +233,14 @@ void app_main (void){
     gpio_config(&io_conf_int2);
 
 
-    xTaskCreate(Parpadeo_led_verde,"Parpadeo_led",2*configMINIMAL_STACK_SIZE,NULL,tskIDLE_PRIORITY + 1,NULL);
-    xTaskCreate(Chequeo_botones,"Chequeo",3*configMINIMAL_STACK_SIZE,(void*)&Control_temporal,tskIDLE_PRIORITY + 1,NULL);
-    xTaskCreate(Contador,"Counter_seg",configMINIMAL_STACK_SIZE,(void*)&Control_temporal,tskIDLE_PRIORITY + 3,NULL);
+    xTaskCreate(Parpadeo_led_verde,NULL,2*configMINIMAL_STACK_SIZE,(void*)Semaforo_Estado_Cronometro,tskIDLE_PRIORITY + 1,NULL);
+    xTaskCreate(Start_Stop_Cronometro,NULL,3*configMINIMAL_STACK_SIZE,(void*)&Control_temporal,tskIDLE_PRIORITY + 1,NULL);
+    xTaskCreate(Reset_Cronometro,NULL,3*configMINIMAL_STACK_SIZE,(void*)&Control_temporal,tskIDLE_PRIORITY + 1,NULL);
+    xTaskCreate(Contador,"Counter_dec",configMINIMAL_STACK_SIZE,(void*)&Control_temporal[0],tskIDLE_PRIORITY + 3,NULL);
+    xTaskCreate(Contador,"Counter_seg",configMINIMAL_STACK_SIZE,(void*)&Control_temporal[2],tskIDLE_PRIORITY + 3,NULL);
+    xTaskCreate(Contador,"Counter_min",configMINIMAL_STACK_SIZE,(void*)&Control_temporal[1],tskIDLE_PRIORITY + 3,NULL);
+
+    
     xTaskCreate(Actualizar_pantalla,"Actualizar_pantalla_dec",16*configMINIMAL_STACK_SIZE,(void*)&Control_temporal,tskIDLE_PRIORITY + 2,NULL);
 
     }
